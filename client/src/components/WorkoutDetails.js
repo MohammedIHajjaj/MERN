6d056fa222318ce6f2e395c364837420857de27d
MohammedIHajjaj <mohammedih31@gmail.com>
2022-07-06 01:28:57 +0300

    Frontend using ReactJS

--- client/src/components/WorkoutDetails.js
+++ client/src/components/WorkoutDetails.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 // const WorkoutDetails = (props) => {
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -12,6 +26,7 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: </strong> {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 };
